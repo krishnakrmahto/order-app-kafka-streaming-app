f721a4344a1c57a76557d4968ae82c565f4b90f1
krishnakrmahto97 <krish.thorcode@gmail.com>
2021-04-21 12:02:39 +0530

    kafka stream processor app with one processing topology that converts kafka msg value to uppercase using string serde

---
+++ src/main/java/com/course/kafka/broker/stream/promotion/PromotionUppercaseStream.java
@@ -0,0 +1,33 @@
+package com.course.kafka.broker.stream.promotion;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.Locale;
+
+@Configuration
+public class PromotionUppercaseStream
+{
+    @Bean
+    public KStream<String, String> kStreamPromotionUppercase(StreamsBuilder streamsBuilder)
+    {
+        // the next 3 lines create a topology for transforming promotion names to uppercase, creating stream (arrow in DAG)
+        // from source topic and sink topic with stream processor (node in DAG) in between.
+        KStream<String, String> sourceStream = streamsBuilder.stream("t.commodity.promotion", Consumed.with(Serdes.String(),
+                                                                                                     Serdes.String()));
+
+        KStream<String, String> uppercaseStream = sourceStream.mapValues((ValueMapper<String, String>) String::toUpperCase);
+
+        uppercaseStream.to("t.commodity.promotion-uppercase", Produced.with(Serdes.String(), Serdes.String()));
+
+        // useful for debugging but don't do the following on production
+        sourceStream.print(Printed.<String, String>toSysOut().withLabel("sourceStream"));
+        uppercaseStream.print(Printed.<String, String>toSysOut().withLabel("uppercaseStream"));
+
+        return sourceStream;
+    }
+}
