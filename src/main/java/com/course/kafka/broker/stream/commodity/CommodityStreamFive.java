071d10bd4b373902ecf8b1a36bcf2902e342a316
krishnakrmahto97 <krish.thorcode@gmail.com>
2021-04-25 09:53:11 +0530

    topology 5 - use forEach to make api call to a third party service for each maskedOrder

--- src/main/java/com/course/kafka/broker/stream/commodity/CommodityStreamFive.java
+++ src/main/java/com/course/kafka/broker/stream/commodity/CommodityStreamFive.java
@@ -4,19 +4,22 @@ import com.course.kafka.broker.message.OrderMessage;
 import com.course.kafka.broker.message.OrderMessageForPattern;
 import com.course.kafka.broker.message.OrderMessageForReward;
 import com.course.kafka.util.CommodityStreamUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.*;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.support.KafkaStreamBrancher;
 import org.springframework.kafka.support.serializer.JsonSerde;
 
 import java.util.Base64;
 
-//@Configuration
-public class CommodityStreamFour
+@Configuration
+@Slf4j
+public class CommodityStreamFive
 {
     @Bean
     public KStream<String, OrderMessage> kStreamOrderMessageFour(StreamsBuilder streamsBuilder)
@@ -32,21 +35,30 @@ public class CommodityStreamFour
 
         // 1. Sink streams - pattern
         new KafkaStreamBrancher<String, OrderMessageForPattern>().branch(getPlasticPredicate(),
-                                                                         kStream -> kStream.to("t.commodity.pattern-four.plastic", orderMessageForPatternProducer))
-                                                                 .defaultBranch(kStream -> kStream.to("t.commodity.pattern-four.notplastic", orderMessageForPatternProducer))
+                                                                         kStream -> kStream.to("t.commodity.pattern-five.plastic", orderMessageForPatternProducer))
+                                                                 .defaultBranch(kStream -> kStream.to("t.commodity.pattern-five.notplastic", orderMessageForPatternProducer))
                                                                  .onTopOf(maskedOrderStream.mapValues(this::toOrderMessageForPattern));
 
         // 2. Sink stream - reward
         maskedOrderStream.filter((key, value) -> value.getQuantity() > CommodityStreamUtil.LARGE_QUANTITY_MIN_VALUE)
                          .filter(getExpensivePredicate())
                          .map(getNewKeyValueMapperForRewardStream())
-                         .to("t.commodity.reward-four", Produced.with(stringSerde, new JsonSerde<>(OrderMessageForReward.class)));
+                         .to("t.commodity.reward-five", Produced.with(stringSerde, new JsonSerde<>(OrderMessageForReward.class)));
 
         // 3. Sink stream - storage
         maskedOrderStream.selectKey(getBase64KeyValueMapper())
-                         .to("t.commodity.storage-four", Produced.with(stringSerde, new JsonSerde<>(OrderMessage.class)));
+                         .to("t.commodity.storage-five", Produced.with(stringSerde, new JsonSerde<>(OrderMessage.class)));
+
+        // 4. Sink (not a sink stream) - fraud --> not creating a down stream, making an api call to fraud detection app
+        maskedOrderStream.filter((key, value) -> value.getOrderLocation().toUpperCase().startsWith("C"))
+                         .foreach(this::reportFraud);
 
         return sourceStream;
+    }
+
+    private void reportFraud(String key, OrderMessage message)
+    {
+        log.info("Reporting fraud to the fraud app using its API call for key: {}, value: {}", key, message);
     }
 
     private KeyValueMapper<String, OrderMessage, String> getBase64KeyValueMapper()
