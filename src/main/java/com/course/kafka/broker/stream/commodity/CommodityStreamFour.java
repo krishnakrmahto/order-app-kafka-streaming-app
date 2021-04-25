1658b1c54df99487f22e305a0e98efd90d8f0574
krishnakrmahto97 <krish.thorcode@gmail.com>
2021-04-25 09:17:46 +0530

    topology 4 - use map to map key as well as value using map() method

--- src/main/java/com/course/kafka/broker/stream/commodity/CommodityStreamFour.java
+++ src/main/java/com/course/kafka/broker/stream/commodity/CommodityStreamFour.java
@@ -6,19 +6,21 @@ import com.course.kafka.broker.message.OrderMessageForReward;
 import com.course.kafka.util.CommodityStreamUtil;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.*;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.support.KafkaStreamBrancher;
 import org.springframework.kafka.support.serializer.JsonSerde;
 
 import java.util.Base64;
 
-//@Configuration
-public class CommodityStreamThree
+@Configuration
+public class CommodityStreamFour
 {
     @Bean
-    public KStream<String, OrderMessage> kStreamOrderMessageThree(StreamsBuilder streamsBuilder)
+    public KStream<String, OrderMessage> kStreamOrderMessageFour(StreamsBuilder streamsBuilder)
     {
         Serde<String> stringSerde = Serdes.String();
 
@@ -31,19 +33,19 @@ public class CommodityStreamThree
 
         // 1. Sink streams - pattern
         new KafkaStreamBrancher<String, OrderMessageForPattern>().branch(getPlasticPredicate(),
-                                                                         kStream -> kStream.to("t.commodity.pattern-three.plastic", orderMessageForPatternProducer))
-                                                                 .defaultBranch(kStream -> kStream.to("t.commodity.pattern-three.notplastic", orderMessageForPatternProducer))
+                                                                         kStream -> kStream.to("t.commodity.pattern-four.plastic", orderMessageForPatternProducer))
+                                                                 .defaultBranch(kStream -> kStream.to("t.commodity.pattern-four.notplastic", orderMessageForPatternProducer))
                                                                  .onTopOf(maskedOrderStream.mapValues(this::toOrderMessageForPattern));
 
         // 2. Sink stream - reward
         maskedOrderStream.filter((key, value) -> value.getQuantity() > CommodityStreamUtil.LARGE_QUANTITY_MIN_VALUE)
                          .filter(getExpensivePredicate())
-                         .mapValues(this::toOrderMessageForReward)
-                         .to("t.commodity.reward-three", Produced.with(stringSerde, new JsonSerde<>(OrderMessageForReward.class)));
+                         .map(getNewKeyValueMapperForRewardStream())
+                         .to("t.commodity.reward-four", Produced.with(stringSerde, new JsonSerde<>(OrderMessageForReward.class)));
 
         // 3. Sink stream - storage
         maskedOrderStream.selectKey(getBase64KeyValueMapper())
-                         .to("t.commodity.storage-three", Produced.with(stringSerde, new JsonSerde<>(OrderMessage.class)));
+                         .to("t.commodity.storage-four", Produced.with(stringSerde, new JsonSerde<>(OrderMessage.class)));
 
         return sourceStream;
     }
@@ -86,5 +88,10 @@ public class CommodityStreamThree
                                     .orderDateTime(orderMessage.getOrderDateTime())
                                     .orderLocation(orderMessage.getOrderLocation())
                                     .build();
+    }
+
+    private KeyValueMapper<String, OrderMessage, KeyValue<String, OrderMessageForReward>> getNewKeyValueMapperForRewardStream()
+    {
+        return (key, value) -> KeyValue.pair(value.getOrderLocation(), toOrderMessageForReward(value));
     }
 }
