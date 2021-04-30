5ee98b636a452cb87779ca2d70f67e1b19a36c32
krishnakrmahto97 <krish.thorcode@gmail.com>
2021-04-30 09:52:08 +0530

    average feedback rating transformer using state store

--- src/main/java/com/course/kafka/broker/stream/feedback/FeedbackStreamSix.java
+++ src/main/java/com/course/kafka/broker/stream/feedback/FeedbackStreamSix.java
@@ -9,13 +9,12 @@ import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KeyValueMapper;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.support.serializer.JsonSerde;
 
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-@Configuration
+//@Configuration
 public class FeedbackStreamSix
 {
     @Bean
