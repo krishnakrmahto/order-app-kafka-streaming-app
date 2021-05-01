5291150106a28d6d575a2ab79e7b9ce9f7b72f63
krishnakrmahto97 <krish.thorcode@gmail.com>
2021-05-01 20:49:04 +0530

    NoArgsConstructor for deserialization

--- src/main/java/com/course/kafka/broker/message/InventoryMessage.java
+++ src/main/java/com/course/kafka/broker/message/InventoryMessage.java
@@ -7,11 +7,13 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 public class InventoryMessage
 {
