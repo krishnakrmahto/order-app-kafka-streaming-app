d01946f72b04a173c79c3d0a0082b31b12260d73
krishnakrmahto97 <krish.thorcode@gmail.com>
2021-05-01 21:36:21 +0530

    reduce instead of aggregate (whenever input and output to aggregate are same, we can use reduce instead)

--- src/main/java/com/course/kafka/broker/stream/inventory/InventoryStreamThree.java
+++ src/main/java/com/course/kafka/broker/stream/inventory/InventoryStreamThree.java
@@ -9,22 +9,25 @@ import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.support.serializer.JsonSerde;
 
-//@Configuration
-public class InventoryStreamTwo
+@Configuration
+public class InventoryStreamThree
 {
     @Bean
     public KStream<String, InventoryMessage> kStreamInventoryTwo(StreamsBuilder streamsBuilder)
     {
         Serde<String> stringSerde = Serdes.String();
+        Serde<Long> longSerde = Serdes.Long();
+
         KStream<String, InventoryMessage> sourceStream = streamsBuilder.stream("t.commodity.inventory",
                                                                                Consumed.with(stringSerde, new JsonSerde<>(InventoryMessage.class)));
 
-        sourceStream.mapValues(value -> "ADD".equals(value.getType())? value.getQuantity(): -value.getQuantity())
+        sourceStream.mapValues(value -> "ADD".equals(value.getType()) ? value.getQuantity() : -value.getQuantity())
                     .groupByKey()
-                    .aggregate(() -> 0L, (key, value, aggregate) -> value + aggregate, Materialized.with(stringSerde, Serdes.Long()))
-                    .toStream().to("t.commodity.inventory-total-two", Produced.with(stringSerde, Serdes.Long()));
+                    .reduce(Long::sum, Materialized.with(stringSerde, longSerde))
+                    .toStream().to("t.commodity.inventory-total-two", Produced.with(stringSerde, longSerde));
 
         return sourceStream;
     }
