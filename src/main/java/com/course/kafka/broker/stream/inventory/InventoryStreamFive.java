e400e21e14151cb5396583a9e98f8f0b2b80c74d
krishnakrmahto97 <krish.thorcode@gmail.com>
2021-05-02 12:13:26 +0530

    tumbling time window

--- src/main/java/com/course/kafka/broker/stream/inventory/InventoryStreamFive.java
+++ src/main/java/com/course/kafka/broker/stream/inventory/InventoryStreamFive.java
@@ -5,21 +5,26 @@ import com.course.kafka.util.InventoryTimestampExtractor;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.kstream.*;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.support.serializer.JsonSerde;
 
-//@Configuration
-public class InventoryStreamFour
+import java.time.Duration;
+
+@Configuration
+public class InventoryStreamFive
 {
     @Bean
-    public KStream<String, InventoryMessage> kStreamInventoryFour(StreamsBuilder streamsBuilder,
+    public KStream<String, InventoryMessage> kStreamInventoryThree(StreamsBuilder streamsBuilder,
                                                                    InventoryTimestampExtractor timestampExtractor)
     {
         Serde<String> stringSerde = Serdes.String();
+        Serde<Long> longSerde = Serdes.Long();
+
         JsonSerde<InventoryMessage> inventoryMessageSerde = new JsonSerde<>(InventoryMessage.class);
+
+        Duration windowLength = Duration.ofHours(1L);
 
         KStream<String, InventoryMessage> sourceStream = streamsBuilder.stream("t.commodity.inventory",
                                                                                Consumed.with(stringSerde,
@@ -27,8 +32,13 @@ public class InventoryStreamFour
                                                                                              timestampExtractor,
                                                                                              null));
 
-        sourceStream.to("t.commodity.inventory-total-four", Produced.with(stringSerde, inventoryMessageSerde));
-
+        Serde<Windowed<String>> windowedSerde = WindowedSerdes.timeWindowedSerdeFrom(String.class, windowLength.toMillis());
+        sourceStream.mapValues(value -> "ADD".equals(value.getType()) ? value.getQuantity() : -value.getQuantity())
+                    .groupByKey()
+                    .windowedBy(TimeWindows.of(windowLength))
+                    .reduce(Long::sum, Materialized.with(stringSerde, longSerde))
+                    .toStream()
+                    .to("t.commodity.inventory-total-five", Produced.with(windowedSerde, longSerde));
         return sourceStream;
     }
 }
